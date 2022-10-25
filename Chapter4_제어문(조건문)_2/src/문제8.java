9a94338f8daefe0440e7c1859bdb852d944b315a
nhk828@naver.com <user@DESKTOP-COJ9858>
2022-10-25 16:56:52 +0900

    22.10.25

---
+++ Chapter4_제어문(조건문)_2/src/문제8.java
@@ -0,0 +1,14 @@
+
+public class 문제8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int even = 0;
+		
+		for(int i=0; i<=100; i+=2) {
+			even += i;
+		}
+		System.out.println("2+4+...100의 합:" +even);
+	}
+
+}

58c9621443d5951c14c6b767531edfbe6a7d880b
nhk828@naver.com <user@DESKTOP-COJ9858>
2022-10-25 16:49:52 +0900

    22.10.25

---
+++ Chapter4_제어문(조건문)_2/src/문제1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class 문제1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수 입력:");
+		int num = sc.nextInt();
+		
+		if(num > 0) {
+			System.out.println(num + "는(은) 양수입니다");
+		} else {
+		System.out.println(num + "는(은) 양수입니다");
+		}
+	}
+
+}

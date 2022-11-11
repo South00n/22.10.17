182e4b09b4fef3e5e40f3cc17162af4d901f04ef
user <nhk828@naver.com>
2022-11-11 17:50:08 +0900

    22.11.11

---
+++ Chapter7_패키지(package_import)/src/com/sist/movie/MovieList.java
@@ -0,0 +1,15 @@
+package com.sist.movie; // 현재 폴더위치 지정
+import com.sist.board.*; // 다른 패키지에 있는 클래스를 사용하기 위해서 import문 사용
+// 라이브러리도 사용하기 위해 import 필수
+/*
+ * 	 import문 생략가능한 라이브러리 = java.lang.* => * 모든 클래스를 불러온다
+ * 							   --------- String, Math, System
+ * 	 -> 기본은 클래스명을 로딩하는것이 좋다
+ * 	 	-> 2개이상 사용할때는 * 써도 무관
+ */
+public class MovieList {
+	BoardList b = new BoardList();
+	void display() {
+	
+	}
+}

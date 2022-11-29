80c992cbe7d1e9b0a65b0561be12c57a9f2c10bc
user <nhk828@naver.com>
2022-11-29 10:29:52 +0900

    22.11.29

---
+++ Chapter15_입출력_3/src/com/sist/io/MainClass_ObjectOutputStream.java
@@ -0,0 +1,13 @@
+package com.sist.io;
+
+import java.io.*;
+import java.util.*;
+// 빅데이터를 저장할때
+public class MainClass_ObjectOutputStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
